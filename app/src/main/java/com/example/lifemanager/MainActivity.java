a9ad322db8e096ac912b791ae4437780d1731462
rwang4 <rwang4@ualberta.ca>
2019-07-27 11:37:15 -0700

    UI note finished

--- app/src/main/java/com/example/lifemanager/MainActivity.java
+++ app/src/main/java/com/example/lifemanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lifemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -78,22 +79,30 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_profile) {
+            // Handle the profile action
+        } else if (id == R.id.nav_note) {
+            // Handle the note action
+            openNoteActivity();
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_money) {
+            // Handle the money action
         }
+//      else if (id == R.id.nav_tools) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void openNoteActivity() {
+        Intent intent = new Intent(this, NoteActivity.class);
+        startActivity(intent);
     }
 }
